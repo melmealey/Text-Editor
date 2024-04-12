d7d06ac8c9bf1cec86f8c17d3fcfa78f209278d1
Mel Mel Mom <melmelmomof4@gmail.com>
2024-04-12 15:31:18 -0400

    npm installs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Text Editor Starter Code
 # Text-Editor
+
+## Description
+I WANT to create notes or code snippets with or without an internet connection
+SO THAT I can reliably retrieve them for later use
+
+## Installation
+
+
+## Usage
+
+
+## Credits
+ * https://webpack.js.org/guides/getting-started/#creating-a-bundle
+ * https://webpack.js.org/concepts/loaders/#configuration
+ * https://webpack.js.org/plugins/mini-css-extract-plugin/#getting-started
+
+* In class activity 
+* ASK BCS & Virtual Learning Assistant
+* Classmates
+* My tutor
+
+## License
+Please refer to the license in my repo
+
+## Links
+* Github Repo: https://github.com/melmealey/Text-Editor
+* Deployed Application: 
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,18 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+//content accepted and added to the dB
+export const putDb = async (content) => {
+  console.log ('PUT to dB');
+  const jateDB = await openDB('jate', 1);
+	const tx = jateDB.transaction('jate', 'readwrite');
+	const store = tx.objectStore('jate');
+	const request = store.put({ id: 1, value: content });
+	const result = await request;
+	console.log('data saved to the dB!!', result);
+  console.error('data not found in the dB');
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (e) => console.error('getDb not implemented');
 
 initdb();
