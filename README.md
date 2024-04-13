7670c4026ed070410a5649a7a6f6f6d4e0bd9f28
Mel Mel Mom <melmelmomof4@gmail.com>
2024-04-13 19:35:10 -0400

    updated README added gitignore

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ I WANT to create notes or code snippets with or without an internet connection
 SO THAT I can reliably retrieve them for later use
 
 ## Installation
-
+Run `npm install` to install all dependencies
+Start the application using `npm run start`
 
 ## Usage
 
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,6 +24,16 @@ export const putDb = async (content) => {
   console.error('data not found in the dB');
 }
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (e) => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDB = await openDB('jate', 1);
+	const tx = jateDB.transaction('jate', 'readonly');
+	const store = tx.objectStore('jate');
+	const request = store.getAll();
+	const result = await request;
+  // return result?.value;
 
+	console.log('data saved to the dB!!', result.value, result);
+  console.error('data not found in the dB');
+}
 initdb();
