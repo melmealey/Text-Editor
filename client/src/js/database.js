66d63c1c2dc3871521bb4d81caa1475044a76d80
Mel Mel Mom <melmelmomof4@gmail.com>
2024-04-15 18:18:03 -0400

    final edits/ removing TODO words

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
 	console.log('data saved to the dB!!', result);
 
 }
-// TODO: Add logic for a method that gets all the content from the database
+//a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDB = await openDB('jate', 1);
@@ -31,9 +31,9 @@ export const getDb = async () => {
 	const store = tx.objectStore('jate');
 	const request = store.getAll();
 	const result = await request;
-  // return result?.value;
 
 	console.log('data saved to the dB!!', result.value, result);
 
 }
+
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Logic for installing the PWA & adding an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   butInstall.style.visability = 'visible';
@@ -9,13 +8,13 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implemented a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => { 
   butInstall.setAttribute ('disabled', true);
   butInstall.textContent = 'Installed!'
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Added a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
  console.log ('appinstalled', event)
  });
